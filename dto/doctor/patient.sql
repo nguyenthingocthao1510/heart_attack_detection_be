a685a66f65e18ee6ea22fa54e150413afde35112
nguyenthingocthao1510 <153586637+nguyenthingocthao1510@users.noreply.github.com>
2024-10-23 09:10:31 +0700

    init medicine sql

--- dto/doctor/patient.sql
+++ dto/doctor/patient.sql
@@ -1,7 +1,8 @@
 CREATE TABLE patient(
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(255),
-)
+    PRIMARY KEY (id)
+) AUTO_INCREMENT = 1;
 
 INSERT INTO `patient` (`id`, `name`) VALUES (6, 'Patient E');
 INSERT INTO `patient` (`id`, `name`) VALUES (5, 'Patient D');
--- dto/main.sql
+++ dto/main.sql
@@ -1,8 +1,17 @@
 -- CREATE TABLE SECTION --
 CREATE TABLE patient(
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(255),
-)
+    PRIMARY KEY (id)
+) AUTO_INCREMENT = 1;
+
+CREATE TABLE medicine (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  uses VARCHAR(255),
+  description VARCHAR(255),
+  PRIMARY KEY (id)
+) AUTO_INCREMENT = 1;
 
 -- INSERT VALUES SECTION --
 INSERT INTO `patient` (`id`, `name`) VALUES (6, 'Patient E');
