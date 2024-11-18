88f16ab9f165a2b078b33f29d4ce27f8d3c397b7
Quách Vinh Quang <hynoras1@gmail.com>
2024-11-18 18:39:53 +0700

    adjust personal information sql

--- dto/patient/personalInformation.sql
+++ dto/patient/personalInformation.sql
@@ -1,9 +1,21 @@
 CREATE TABLE patient(
-    patient_id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     account_id INT NOT NULL,
-    patient_name VARCHAR(255),
-    patient_gender varchar(10),
-    patient_dob DATE,
+    name VARCHAR(255),
+    gender varchar(10),
+    dob DATE,
     PRIMARY KEY (patient_id)
     FOREIGN KEY (account_id) REFERENCES account(id)
 ) AUTO_INCREMENT = 1;
+
+DROP TABLE patient;
+
+INSERT INTO account (
+
+ALTER TABLE patient ADD COLUMN account_id INT NOT NULL;
+ALTER TABLE patient ADD CONSTRAINT account_ibfk_2 FOREIGN KEY (account_id) REFERENCES account(id);
+
+UPDATE patient SET account_id = 16 WHERE id IN (6,5,4,3);
+
+ALTER TABLE patient ADD COLUMN pt_gender VARCHAR(10);
+ALTER TABLE patient ADD COLUMN pt_dob DATE;
