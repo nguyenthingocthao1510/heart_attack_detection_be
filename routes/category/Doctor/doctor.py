5be91e75b730da3951aba5796e11f7a6f22e581e
nguyenthingocthao1510 <153586637+nguyenthingocthao1510@users.noreply.github.com>
2024-11-27 22:06:10 +0700

    Create feature doctor

--- routes/category/Doctor/doctor.py
+++ routes/category/Doctor/doctor.py
@@ -1,7 +1,23 @@
 from flask import Blueprint
-from controllers.category.Doctor.doctor import get_all
+from controllers.category.Doctor.doctor import get_all, get_by_id, create, update, delete
 
 doctor_route = Blueprint('doctor_route', __name__)
 @doctor_route.route('/doctors', methods = ['GET'])
 def get_all_doctor():
     return get_all()
+
+@doctor_route.route('/doctor/accountId=<int:account_id>', methods = ['GET'])
+def get_doctor_by_id(account_id):
+    return get_by_id(account_id)
+
+@doctor_route.route('/doctor/create-information', methods = ['POST'])
+def add_doctor():
+    return create()
+
+@doctor_route.route('/doctor/update-information/doctorId=<int:id>', methods = ['PUT'])
+def update_doctor(id):
+    return update(id)
+
+@doctor_route.route('/doctor/delete-information/doctorId=<int:id>', methods = ['DELETE'])
+def delete_doctor(id):
+    return delete(id)
